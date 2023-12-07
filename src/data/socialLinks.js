32bd17a7cbad8723bfa8fca7a6068d8f62195f02
Boris Theron <boris.theron@etu.umontpellier.fr>
2023-12-07 17:28:48 +0100

    1st commit

---
+++ src/data/socialLinks.js
@@ -0,0 +1,32 @@
+export const socialLinks = [
+	{
+		id: 1,
+		name: 'Website',
+		icon: 'globe',
+		url: 'https://www.theronb.me/',
+	},
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/nebbiuu',
+	},
+	{
+		id: 3,
+		name: 'Twitter',
+		icon: 'twitter',
+		url: 'https://twitter.com/nebbiuu',
+	},
+	{
+		id: 4,
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/in/boris-theron-42033b231',
+	},
+	{
+		id: 5,
+		name: 'YouTube',
+		icon: 'youtube',
+		url: 'https://www.youtube.com/c/UCJ9Fi68Joz81GP67bEIfQvw',
+	},
+];
